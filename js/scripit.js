dcda517f2ff2a7ac81a04e425a6d4fe95e18a14c
Warleson Candido da Silva <warllesoncandido@gmail.com>
2019-04-15 08:17:46 -0300

    commit 2.4

--- js/scripit.js
+++ js/scripit.js
@@ -3,3 +3,8 @@ document.getElementById('projeto').addEventListener('click',function(){
     window.open('projetos.html',"_blank_","toolbar=false, scrollbars=false,resizable=false, top=200, left=500, width=500, height=600");
 
 });
+document.getElementById('contatos').addEventListener('click',function(){
+
+    window.open('contatos.html',"_blank_","toolbar=false, scrollbars=false,resizable=false, top=200, left=500, width=500, height=600");
+
+});
