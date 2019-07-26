24ba4922b912e06cdf10df57182e5e5c32051bb1
Warleson Candido da Silva <warllesoncandido@gmail.com>
2019-07-26 14:28:55 -0300

    Adicionando mais um projeto

--- js/scripit.js
+++ js/scripit.js
@@ -8,3 +8,4 @@ document.getElementById('contatos').addEventListener('click',function(){
     window.open('contatos.html',"_blank_","toolbar=false, scrollbars=false,resizable=false, top=200, left=500, width=500, height=600");
 
 });
+
